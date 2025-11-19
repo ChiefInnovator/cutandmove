26954b28e56e2d8f2d1eb74748bf6f642d9d8503
Richard Crane <rich@mill5.com>
2025-11-19 08:45:29 -0800

    Initial Commit

---
+++ CutAndMove/CutAndMoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  CutAndMoveApp.swift
+//  CutAndMove
+//
+//  Created by Richard Crane on 11/19/25.
+//
+
+import SwiftUI
+
+@main
+struct CutAndMoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CutAndMoveTests/CutAndMoveTests.swift
@@ -0,0 +1,17 @@
+//
+//  CutAndMoveTests.swift
+//  CutAndMoveTests
+//
+//  Created by Richard Crane on 11/19/25.
+//
+
+import Testing
+@testable import CutAndMove
+
+struct CutAndMoveTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
